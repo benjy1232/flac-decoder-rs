383d41d481492b09d1b820faee859405285099ff
Ben Serrano <b.serrano8970@gmail.com>
2024-11-30 22:25:40 -0600

    Add MIT License

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) Ben Serrano. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information
+ */
 use core::fmt;
 use std::{
     fs,
